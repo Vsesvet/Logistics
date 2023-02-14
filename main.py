cb3c957b33b647bb25f849e5f0d9a6f8ff3a2deb
Vsesvet <vsesvet@bk.ru>
2023-02-14 23:42:20 +0300

    1. Добавлено окно Логина 2. Добавлено окно Расписания мероприятий (не все кнопки)

--- main.py
+++ main.py
@@ -1,13 +1,127 @@
 #!/usr/bin/env/ python3
-# -*- coding: utf-8 -*-
-# Изначально вручную - создаем директорию '/home/logistics'. И присваиваем ей права пользователя и группы.
-# tail -f /home/logistics/journal.log - динамическое отображение логов работы программы.
-# Установка PyMySQL: sudo pip3 install PyMySQL
+
 import os
 import sys
 from datetime import datetime
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 #import pymysql
 
+
+class Ui_Login(QWidget):
+    def __init__(self):
+        super(Ui_Login, self).__init__()
+        self.setObjectName("Login")
+        self.resize(365, 215)
+        self.pushButton_login = QtWidgets.QPushButton(self)
+        self.pushButton_login.setGeometry(QtCore.QRect(140, 180, 89, 25))
+        self.pushButton_login.setObjectName("pushButton_login")
+        self.label_main_input = QtWidgets.QLabel(self)
+        self.label_main_input.setGeometry(QtCore.QRect(130, 0, 131, 31))
+        font = QtGui.QFont()
+        font.setPointSize(17)
+        self.label_main_input.setFont(font)
+        self.label_main_input.setObjectName("label_main_input")
+        self.formLayoutWidget = QtWidgets.QWidget(self)
+        self.formLayoutWidget.setGeometry(QtCore.QRect(110, 50, 241, 104))
+        self.formLayoutWidget.setObjectName("formLayoutWidget")
+        self.formLayout = QtWidgets.QFormLayout(self.formLayoutWidget)
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.formLayout.setObjectName("formLayout")
+        self.label_password = QtWidgets.QLabel(self.formLayoutWidget)
+        self.label_password.setObjectName("label_password")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.label_password)
+        self.label_bad_password = QtWidgets.QLabel(self.formLayoutWidget)
+        self.label_bad_password.setText("")
+        self.label_bad_password.setObjectName("label_bad_password")
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.FieldRole, self.label_bad_password)
+        self.lineEdit_password = QtWidgets.QLineEdit(self.formLayoutWidget)
+        self.lineEdit_password.setObjectName("lineEdit_password")
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.lineEdit_password)
+        self.lineEdit_login = QtWidgets.QLineEdit(self.formLayoutWidget)
+        self.lineEdit_login.setObjectName("lineEdit_login")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.lineEdit_login)
+        self.label_login = QtWidgets.QLabel(self.formLayoutWidget)
+        self.label_login.setObjectName("label_login")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label_login)
+        self.label_user_not_found = QtWidgets.QLabel(self.formLayoutWidget)
+        self.label_user_not_found.setText("")
+        self.label_user_not_found.setObjectName("label_user_not_found")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.label_user_not_found)
+
+        self.retranslateUi(self)
+        self.pushButton_login.clicked.connect(self.close) # type: ignore
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def retranslateUi(self, Login):
+        _translate = QtCore.QCoreApplication.translate
+        Login.setWindowTitle(_translate("Login", "Вход в программу"))
+        self.pushButton_login.setText(_translate("Login", "Войти"))
+        self.label_main_input.setText(_translate("Login", "Логистик"))
+        self.label_password.setText(_translate("Login", "Пароль"))
+        self.lineEdit_login.setPlaceholderText(_translate("Login", "79265002010"))
+        self.label_login.setText(_translate("Login", "Логин"))
+
+    def move_to_center(self):
+        desktop = QApplication.desktop()
+        rect = desktop.screenGeometry()
+        x = (desktop.width() - self.width()) // 2
+        y = (desktop.height() - self.height()) // 2
+        self.move(x, y)
+
+class Ui_Event_shedule(QMainWindow):
+    def __init__(self):
+        super(Ui_Event_shedule, self).__init__()
+        self.setObjectName("Event_shedule")
+        self.resize(1079, 600)
+        self.setLayoutDirection(QtCore.Qt.RightToLeft)
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton_create_event = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_create_event.setGeometry(QtCore.QRect(20, 90, 171, 25))
+        self.pushButton_create_event.setObjectName("pushButton_create_event")
+        self.pushButton_create_organization = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_create_organization.setGeometry(QtCore.QRect(210, 90, 171, 25))
+        self.pushButton_create_organization.setObjectName("pushButton_create_organization")
+        self.pushButton_create_member = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_create_member.setGeometry(QtCore.QRect(20, 130, 171, 25))
+        self.pushButton_create_member.setObjectName("pushButton_create_member")
+        self.label_username_role = QtWidgets.QLabel(self.centralwidget)
+        self.label_username_role.setGeometry(QtCore.QRect(750, 0, 321, 20))
+        self.label_username_role.setText("")
+        self.label_username_role.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label_username_role.setObjectName("label_username_role")
+        self.label_event_shedule = QtWidgets.QLabel(self.centralwidget)
+        self.label_event_shedule.setGeometry(QtCore.QRect(380, 20, 321, 31))
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        self.label_event_shedule.setFont(font)
+        self.label_event_shedule.setObjectName("label_event_shedule")
+        self.tree_event_shedule = QtWidgets.QTreeWidget(self.centralwidget)
+        self.tree_event_shedule.setGeometry(QtCore.QRect(10, 180, 1061, 221))
+        self.tree_event_shedule.setObjectName("tree_event_shedule")
+        self.tree_event_shedule.headerItem().setText(0, "1")
+        self.pushButton_create_insector = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_create_insector.setGeometry(QtCore.QRect(210, 130, 171, 25))
+        self.pushButton_create_insector.setObjectName("pushButton_create_insector")
+        self.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(Ui_Event_shedule)
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def retranslateUi(self, Ui_Event_shedule):
+        _translate = QtCore.QCoreApplication.translate
+        self.setWindowTitle(_translate("Event_shedule", "Логистик (Event shedule)"))
+        self.pushButton_create_event.setText(_translate("Event_shedule", "Создать мероприятие"))
+        self.pushButton_create_organization.setText(_translate("Event_shedule", "Создать организацию"))
+        self.pushButton_create_member.setText(_translate("Event_shedule", "Создать участника"))
+        self.label_event_shedule.setText(_translate("Event_shedule", "Расписание мероприятий"))
+        self.pushButton_create_insector.setText(_translate("Event_shedule", "Создать инспектора"))
+
+    def show_login(self):
+        self.wind_login = Ui_Login()
+        self.wind_event_shedule = Ui_Event_shedule()
+        self.wind_login.pushButton_login.clicked.connect(self.wind_event_shedule.show)
 
 class Processing:
     """Класс обработки данных"""
@@ -28,6 +142,7 @@ class Processing:
     def rename_file_client(self, documents_members):
         """Переименование документов загруженных участниками мероприятия"""
         pass
+
 
 class User:
     """Класс создания, редактирования, удаления, сохранения пользователя в БД"""
@@ -72,6 +187,7 @@ class User:
         self.user_profile_folder = f'/home/logistics/{self.full_name}_{self.phone}'
         os.makedirs(self.user_profile_folder)
         journal.log(f'Создана профильная директория: {self.full_name}_{self.phone}')
+
 
 class Access:
     """Access module. Проверка данных Входа и роли пользователя, для совершения действия"""
@@ -209,6 +325,16 @@ survey = 'survey'
 documents_members = (agree, diplom, inn, passport, registration, snils, sertificate, survey)
 
 # Вход пользователя по номеру телефона и паролю, запуск программы
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    login = Ui_Login()
+    login.move_to_center()
+    login.show()
+    # event_shedule = Ui_Event_shedule()
+    # event_shedule.show()
+    sys.exit(app.exec())
+
+
 number = input('Логин: ')
 password = input('Пароль: ')
 user_login = access.login(number, password)
