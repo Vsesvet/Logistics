5851212e7bcea9a74169da2fdd410c21a6001bd4
IlyaFedorov7 <fidson5844@>
2023-02-22 20:44:04 +0300

    Добавил функции: удаление участника, select* from table_name

--- Class_Mysql.py
+++ Class_Mysql.py
@@ -24,29 +24,45 @@ class Mysql:
 
     def add_participant(self, phone_number, second_name, first_name, last_name, role, full_name, city, email, password, comment,disabled):
         """Добавление нового пользователя в базу данных MySql"""
-        print(phone_number, second_name, first_name, last_name, role, full_name, city, email, password, comment)
+
         select_role_id = f"SELECT role_id FROM roles WHERE  role_name = '{role}'"
         with self.connection.cursor() as cursor:
             cursor.execute(select_role_id)
             result = cursor.fetchall()
             self.connection.commit()
-        print(result)
+
         role_id = result[0]['role_id']
-        print(role_id)
 
         insert_query = f"INSERT INTO participants (phone_number, second_name, first_name, last_name, full_name, role_id, city, email, password, comment,disabled) \
         VALUES('{phone_number}', '{second_name}', '{first_name}', '{last_name}', '{full_name}', {role_id}, '{city}', '{email}', '{password}', '{comment}',{disabled})"
 
-        print(insert_query)
         with self.connection.cursor() as cursor:
             cursor.execute(insert_query)
             self.connection.commit()
 
-    def delete_user(self, id):
-        """Удаление пользователя из БД MySql по id"""
-        delete_quere = f"DELETE FROM 'users' WHERE id = {id}"
+    def select_all(self, table_name):
+        """Выбор всех строк из таблицы. Возвращает ? """
+        select_all = f"SELECT * FROM {table_name}"
         with self.connection.cursor() as cursor:
-            cursor.execute(delete_quere)
+            cursor.execute(select_all)
+            result = cursor.fetchall()
+            self.connection.commit()
+        print(result)
+        return result
+
+    def get_participant_id(self, phone_number):
+        select_id = f"SELECT participant_id FROM participants WHERE phone_number = {phone_number}"
+        with self.connection.cursor() as cursor:
+            cursor.execute(select_id)
+            result = cursor.fetchall()
+            self.connection.commit()
+        print(result)
+        return result
+    def delete_participant(self, id):
+        """Удаление пользователя из БД MySql по id"""
+        delete_quere = f"DELETE FROM participants WHERE participant_id = {id}"
+        with self.connection.cursor() as cursor:
+            print(cursor.execute(delete_quere))
             self.connection.commit()
 
     def __del__(self):
